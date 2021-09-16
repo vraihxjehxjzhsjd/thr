c2e1fd2eb01fb2c03d982d0c7ecff1a03fa28b8d
vraihxjehxjzhsjd <90188100+vraihxjehxjzhsjd@users.noreply.github.com>
2021-09-16 13:01:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ global count
 count = 0
 
 def start_client(app):
-    app.start()
-    me = app.get_me()
-    print(f"Log as @{me.username}")
+    with app:
+        me = app.get_me()
+        print(f"Log as @{me.username}")
     @app.on_message(filters.command("а", "а"))
     async def flood(client, message):
         try:
@@ -21,8 +21,8 @@ def start_client(app):
             except:
                 pass
             print(f"sended. COUNT: {count}")
+    app.run()
 
-input("start? [y/n] (y): ")
 clients = [pyrogram.Client("1", bot_token="1951351434:AAF6robWwzaRZ7FMmBEE9NEW22IQOEUXGcw"), pyrogram.Client("2", bot_token="1920365445:AAGLxcah_iX2GePwSvxo-sc8VX2ZRrxiwSI")]
 for client in clients:
     threading.Thread(start_client(app=client)).start()
