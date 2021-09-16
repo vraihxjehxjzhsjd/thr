3fe600d5ff5d893ccc7b71b97fb9761c49fce087
vraihxjehxjzhsjd <90188100+vraihxjehxjzhsjd@users.noreply.github.com>
2021-09-16 12:55:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ global count
 count = 0
 
 def start_client(app):
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
     app.start()
     me = app.get_me()
     print(f"Log as @{me.username}")
@@ -26,7 +24,7 @@ def start_client(app):
 input("start? [y/n] (y): ")
 clients = [pyrogram.Client("1", bot_token="1951351434:AAF6robWwzaRZ7FMmBEE9NEW22IQOEUXGcw"), pyrogram.Client("2", bot_token="1920365445:AAGLxcah_iX2GePwSvxo-sc8VX2ZRrxiwSI")]
 for client in clients:
-    threading.Thread(target=start_client, args=(client,)).start()
+    threading.Thread(start_client(app=client)).start()
     print(f"send 'ок' to chat")
     pyrogram.idle()
             
